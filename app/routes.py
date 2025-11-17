c80e0311eaa1b8abd95be5a97c73c7bad5cc70f6
BrianHeT <129415133+BrianHeT@users.noreply.github.com>
2025-11-17 03:53:29 -0300

    implementacion google-auth

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask import Blueprint, render_template, redirect, url_for, flash, request, current_app
 from flask_login import login_user, logout_user, login_required, current_user
-from . import database, bcrypt   
+from . import database, bcrypt
 from .models import User, Book
-from .forms import RegistrationForm 
+from .forms import RegistrationForm
+import json
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+from urllib.parse import urlencode, urlunparse
+
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
 
 # Crear el blueprint
 bp = Blueprint('main', __name__)
@@ -69,6 +80,81 @@ def login():
 
     return render_template('login.html', title='Iniciar Sesión') 
 
+@bp.route("/login/google") 
+def login_google():
+    if current_user.is_authenticated:
+        
+        return redirect(url_for('main.index'))
+    
+    
+    client = WebApplicationClient(current_app.config['GOOGLE_CLIENT_ID'])
+    
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url.replace("login/google", "callback/google"),
+        
+        scope=current_app.config['GOOGLE_SCOPE'], 
+        prompt="select_account"
+    )
+    return redirect(request_uri)
+
+
+@bp.route("/callback/google")
+def callback_google():
+    
+    client = WebApplicationClient(current_app.config['GOOGLE_CLIENT_ID'])
+    
+    code = request.args.get("code")
+    
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        
+        auth=(current_app.config['GOOGLE_CLIENT_ID'], current_app.config['GOOGLE_CLIENT_SECRET'])
+    )
+
+    client.parse_request_body_response(token_response.text)
+    
+    
+    uri, headers, body = client.add_token(current_app.config['GOOGLE_USER_INFO_URL'])
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+    
+    if userinfo_response.json().get("email_verified"):
+        google_id = userinfo_response.json()["id"]
+        email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        name = userinfo_response.json()["name"]
+        
+        user = User.query.filter_by(email=email).first()
+        
+        if not user:
+            
+            user = User(name=name, email=email, password_hash='oauth_user') 
+            database.session.add(user) 
+            database.session.commit()
+            flash('¡Cuenta de Google registrada con éxito!', 'success')
+        
+        login_user(user)
+        
+        return redirect(url_for('main.index'))
+    else:
+        flash("El email de Google no pudo ser verificado.", 'danger')
+        
+        return redirect(url_for('main.login'))
+    
 # -------------------- CIERRE DE SESIÓN --------------------
 
 @bp.route("/logout")
