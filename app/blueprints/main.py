cef9047d08b923f0d55922d7c9febbebaa674d52
RafaelMaciasT <maciasjesus03@gmail.com>
2025-11-18 22:27:50 -0300

    ruta_soporte
    ruta hacia el soporte en el footer

---
+++ app/blueprints/main.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+# Crear el blueprint
+bp_main = Blueprint('main', __name__)
+# -------------------- RUTAS PÚBLICAS --------------------
+@bp_main.route("/")
+def index():
+    return render_template('index.html', title='Inicio')
+
+@bp_main.route("/about")
+def about():
+    return render_template('about.html', title='Acerca de')
+@bp_main.route("/soporte")
+def soporte():
+    return render_template('soporte.html', title='Soporte')
