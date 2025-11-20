5b2f024d0b515d96cfd32a1b7e76a95f7d3459f8
BrianHeT <129415133+BrianHeT@users.noreply.github.com>
2025-11-19 22:20:29 -0300

    fix index y detalles libro

--- app/blueprints/main.py
+++ app/blueprints/main.py
@@ -1,11 +1,26 @@
 from flask import Blueprint, render_template
+from app.models import Book 
+from flask import current_app
 
 # Crear el blueprint
 bp_main = Blueprint('main', __name__)
 # -------------------- RUTAS PÚBLICAS --------------------
 @bp_main.route("/")
 def index():
-    return render_template('index.html', title='Inicio')
+    all_books = Book.query.order_by(Book.id.asc()).limit(10).all()
+
+    mas_vendidos = all_books[:5]  
+    recomendados = all_books[5:10] 
+    
+    s3_config = {
+        'BASE_URL': f"https://{current_app.config.get('AWS_S3_BUCKET')}.s3.{current_app.config.get('AWS_REGION')}.amazonaws.com/"
+    }
+    
+    return render_template('index.html', 
+                           title='Inicio', 
+                           mas_vendidos=mas_vendidos,
+                           recomendados=recomendados,
+                           s3_config=s3_config)
 
 @bp_main.route("/about")
 def about():
